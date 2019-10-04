be0e0b77e342ee4ba7bcb60411a445c391ff474e
SafelinkURL <54962933+SafelinkURL@users.noreply.github.com>
2019-10-04 19:44:26 +0700

    Create postbody.js

---
+++ postbody.js
@@ -0,0 +1,24 @@
+
+<script type='text/javascript'>
+
+//<![CDATA[
+
+var pre = document.getElementsByAttribute("POSTBODY");
+
+
+
+Object.values(pre).forEach((x)=>{
+
+var att = document.createAttribute("class");
+
+att.value = "postbody toc";
+
+x.setAttributeNode(att);
+
+})
+
+
+
+//]]>
+
+</script>
